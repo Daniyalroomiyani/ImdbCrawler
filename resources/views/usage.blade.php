8a5a59ccbff0e7968eda7630873fd1df07fb872d
daniyal <daniyal_back@yahoo.com>
2022-12-03 01:02:37 +0330

    getting start on Host

---
+++ resources/views/usage.blade.php
@@ -0,0 +1,49 @@
+<!-- ======= usage Section ======= -->
+<section id="usage" class="usage">
+    <div class="container">
+
+        <div class="section-title">
+            <h2>send a Crawler</h2>
+            <p>
+                Simply Fill IMDb ID and Type of date you want and then it Crawls
+            </p>
+        </div>
+
+        <div class="row" data-aos="fade-in">
+
+            <div class="col-lg-5">
+                <form>
+                    <div class="row mb-3">
+                        <label for="id" class="col-sm-2 col-form-label">IMDb-ID</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="id">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="id" class="col-sm-2 col-form-label">IMDb-ID</label>
+                        <div class="col-sm-10">
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Select a data type</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                    </div>
+
+
+                    <div class="d-grid gap-2 col-6 mx-auto">
+
+                        <button class="btn btn-secondary" type="button">send</button>
+                    </div>
+                </form>
+
+            </div>
+
+            <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+            </div>
+
+        </div>
+
+    </div>
+</section><!-- End usage Section -->
